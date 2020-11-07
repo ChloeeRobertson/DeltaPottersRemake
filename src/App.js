a052134ef61013f6bc3e4482d3dfb7845a96c0a2
Chloee Robertson <me.c.leigh@gmail.com>
2020-11-06 21:53:30 -0800

    Changes to colors and layout of home page

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ import {Container, Row, Col} from 'react-bootstrap';
 
 function App() {
   return (
-      <Container>
+      <Container fluid style={{backgroundColor: "#fdf7f2"}}>
         <Navbar/>
-        <HomeGallery/>
-        <Container>
-          <Row>
-            <Col lg={2}></Col>
-            
-            <Col lg={8}>
-              <h1 className="text-center">WELCOME!</h1>
-              <p className="text-center"><strong>Delta Potters</strong> is made up of a group of local artisans and nascent potters who create functional and artistic pots while learning new skills in a co-operative environment. We come together as individuals from diverse backgrounds and operate from the premise that we will offer help to one another, share ideas, learn new skills in the craft of pottery, and support the ongoing success of our guild. We began as a non-profit organization in 1972.</p>
-            </Col>
-          </Row>
-        </Container>
-        <Jumbo/>
+        <Row> 
+          <Col lg={6}>
+            <HomeGallery/>
+          </Col>
+          <Col lg={6}>
+            <Row>
+              <Col lg={8}>
+                <h1 className="text-center">WELCOME!</h1>
+                <p className="text-center"><strong>Delta Potters</strong> is made up of a group of local artisans and nascent potters who create functional and artistic pots while learning new skills in a co-operative environment. We come together as individuals from diverse backgrounds and operate from the premise that we will offer help to one another, share ideas, learn new skills in the craft of pottery, and support the ongoing success of our guild. We began as a non-profit organization in 1972.</p>
+              </Col>
+            </Row>
+            <Row>
+              <Jumbo/>
+            </Row>
+          </Col>
+        </Row>
       </Container>
   );
 }
--- src/components/membership-jumbo.jsx
+++ src/components/membership-jumbo.jsx
@@ -3,8 +3,8 @@ import {Jumbotron, Container} from 'react-bootstrap';
 function Jumbo() {
     
   return (
-    <Jumbotron fluid>
-      <Container>
+    <Jumbotron fluid style={{backgroundColor: "#F18C8E"}}>
+      <Container style={{backgroundColor: "#F18C8E"}}>
         <h1 >Fluid jumbotron</h1>
         <p>
           This is a modified jumbotron that occupies the entire horizontal space of
